f450f9598f569bfbc82db07ef56cd5fa66727c93
Set catalan <2024_set.catalan@iticbcn.cat>
2025-11-06 18:40:10 +0100

    Final activitat

--- 06-Factory/src/iticbcn/xifratge/XifradorRotX.java
+++ 06-Factory/src/iticbcn/xifratge/XifradorRotX.java
@@ -59,31 +59,38 @@ public class XifradorRotX implements Xifrador {
     @Override
     public TextXifrat xifra(String msg, String clau) throws ClauNoSuportada {
         int clauInt;
+        TextXifrat textXifrat = null;
         try {
             clauInt = Integer.parseInt(clau);
-            if (clauInt < 0 && clauInt > 40){
+            if (clauInt >= 0 && clauInt <= 40){
+                byte[] bmsg = xifraRotX(msg.toCharArray(), clauInt).getBytes();
+                textXifrat = new TextXifrat(bmsg);
+            } else {
                 throw new ClauNoSuportada("Clau de RotX ha de ser un sencer de 0 a 40");
             }
         } catch (Exception e) {
             throw new ClauNoSuportada("Clau de RotX ha de ser un sencer de 0 a 40");
         }
-        byte[] bmsg = xifraRotX(msg.toCharArray(), clauInt).getBytes();
-        TextXifrat textXifrat = new TextXifrat(bmsg);
+        
         return textXifrat;
     }
 
     @Override
     public String desxifra(TextXifrat xifrat, String clau) throws ClauNoSuportada {
         int clauInt;
+        String msg = null;
         try {
             clauInt = Integer.parseInt(clau);
-            if (clauInt < 0 && clauInt > 40){
+            if (clauInt >= 0 && clauInt <= 40){
+                String msgXifrat = xifrat.toString();
+                msg = desxifraRotX(msgXifrat.toCharArray(), clauInt);
+            } else {
                 throw new ClauNoSuportada("Clau de RotX ha de ser un sencer de 0 a 40");
             }
         } catch (Exception e) {
             throw new ClauNoSuportada("Clau de RotX ha de ser un sencer de 0 a 40");
         }
-        String msgXifrat = xifrat.toString();
-        return desxifraRotX(msgXifrat.toCharArray(), clauInt);
+        
+        return msg;
     }
 }
